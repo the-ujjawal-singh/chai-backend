c2506088de9340c4eb93c8047582bc93be3c032e
the-ujjawal-singh <ujjwalsingh97080@gmail.com>
2025-03-24 14:14:47 +0530

    Add refreshAccessToken endpoint

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,7 +62,7 @@ userSchema.methods.isPasswordCorrect = async function(password){
 }
 
 
-userSchema.methods.generateToken = function(){
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
             _id: this._id,
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,7 +14,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         //upload file to cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {resource_type: "auto"})
         //file has been uploaded successfully
-        console.log("file is uploaded on cloudinary ", response.url);
+        // console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file
         return response;
         
     } catch (error) {
